75d913f69fa4a1039e88cd650224ad7660d51e41
hanzala-bhutto <bhutto.hanzala@gmail.com>
2023-10-30 20:32:46 +0500

    assets file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Configure your color palette here
+        tomato : 'hsl(4, 100%, 67%)',
+        darkSlate: 'hsl(234, 29%, 20%)',
+        cusGray: 'hsl(231, 7%, 60%)',
+        charcoalGray: 'hsl(235, 18%, 26%)'
+      },
+    },
   },
   plugins: [],
 }
